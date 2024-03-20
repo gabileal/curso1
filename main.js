9b04f9c23e3b1010847ebce939138646f6e2c5e1
edson gabriel <106999196+gabileal@users.noreply.github.com>
2024-03-20 10:49:35 -0300

    20/03

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
 
-for(let i-0;i<botoes.length;i++){}
+for(let i-0;i<botoes.length;i++){
+    botoes[i].onclick = fuction(){
+        botoes[1]. classList.add('ativo');
+    }
+    console.log(i);
+}
