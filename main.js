08e4b83667f6b06bb31c8a4d43f6d05abcdac064
edson gabriel <106999196+gabileal@users.noreply.github.com>
2024-04-17 11:29:02 -0300

    17/04

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-const contadores = document.querySelectorAll(".contador");
+//Código omitido
 
-contadores[0].textContent = "contagem regressiva";
+const contadores = document.querySelectorAll(".contador"); 
+const tempoObjetivo1 = new Date("2024-10-05"); 
+
+contadores[0].textContent = tempo0bjetivo1; 
