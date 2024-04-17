4ef6107260e537f368cb1066a1d6932fe086ef8c
edson gabriel <106999196+gabileal@users.noreply.github.com>
2024-04-17 11:34:05 -0300

    17/04

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-//Código omitido
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
 
-const contadores = document.querySelectorAll(".contador"); 
-const tempoObjetivo1 = new Date("2024-10-05"); 
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
-contadores[0].textContent = tempo0bjetivo1; 
